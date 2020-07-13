b7eaac4fc72003ba9a174050555d0ab1b8e49a5e
pinkninjajess <jessica.valenti1@gmail.com>
2020-07-13 09:04:23 -0400

    Allowing cross origin from front-end Angular app and disabling certain HTTP requests

---
+++ src/main/java/ca/jessicavalenti/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package ca.jessicavalenti.ecommerce.config;
+
+import ca.jessicavalenti.ecommerce.entity.Product;
+import ca.jessicavalenti.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.POST};
+
+        // disable HTTP methods for Product: PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+    }
+}
